8b38784409c5331648234302e53f1aa265ab1989
MD <dani.super@gmail.com>
2015-11-09 03:18:37 +0100

    separated login from app, redirection changed

---
+++ login/User.ts
@@ -0,0 +1,12 @@
+/**
+ * Created by S on 2015.11.09..
+ */
+
+export class User {
+
+    constructor(private username:string,
+        private password:string,
+        private email:string,
+        private token:string) {
+    }
+}
